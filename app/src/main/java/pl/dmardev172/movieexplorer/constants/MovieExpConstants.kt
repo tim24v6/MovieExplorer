c89557aca66a30f047968f55961f7ee947b39dc5
dmardev172 <dmar.dev2703@gmail.com>
2026-03-03 19:05:10 +0100

    Extract base url (#6)

---
+++ app/src/main/java/pl/dmardev172/movieexplorer/constants/MovieExpConstants.kt
@@ -0,0 +1,5 @@
+package pl.dmardev172.movieexplorer.constants
+
+object MovieExpConstants {
+    const val BASE_URL = "https://www.omdbapi.com"
+}
--- app/src/main/java/pl/dmardev172/movieexplorer/di/NetworkModule.kt
+++ app/src/main/java/pl/dmardev172/movieexplorer/di/NetworkModule.kt
@@ -1,6 +1,7 @@
 package pl.dmardev172.movieexplorer.di
 
 import org.koin.dsl.module
+import pl.dmardev172.movieexplorer.constants.MovieExpConstants
 import pl.dmardev172.movieexplorer.data.remote.OmdbApi
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -9,8 +10,7 @@ val networkModule = module {
 
     single {
         Retrofit.Builder()
-            .baseUrl("https://www.omdbapi.com/")
-//            .client(get())
+            .baseUrl(MovieExpConstants.BASE_URL)
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
     }
